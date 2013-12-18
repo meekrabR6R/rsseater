d0ad0f2cded8fbe492cbcd15c216b333af00773b
meekrab <nmiano84@gmail.com>
2013-12-17 22:24:08 -0500

    Tests for GoogleFeedFinder

--- src/test/scala/FinderSpec.scala
+++ src/test/scala/FinderSpec.scala
@@ -1,9 +1,14 @@
 package com.meekrab.rsseater
 
 import org.scalatest.FunSpec
+import play.api.libs.json._
+import play.api.libs.functional._
 
 trait Tester {
-  val feeds = GoogleFeedFinder("Skyrim")
+
+  val feeds = GoogleFeedFinder("Skyrim")  
+  val testJson = scala.io.Source.fromFile("src/test/assets/test.json").mkString
+  val testFeeds = GoogleFeedFinder.FeedList(Json.parse(testJson))
 }
 
 class FinderSpec extends FunSpec {
@@ -12,6 +17,14 @@ class FinderSpec extends FunSpec {
       it("should = 'Skyrim'"){
         assert(feeds.query === "Skyrim")
       }
+    }
+    describe("First feed") {
+    	it("url should = 'http://cms.elderscrolls.com/feeds/rss/index.php?locale\u003den-us'") {
+    		assert(testFeeds.entries.head.url === "http://cms.elderscrolls.com/feeds/rss/index.php?locale\u003den-us")
+    	}
+    	it("title should = 'The Elder Scrolls Official Site | \u003cb\u003eSkyrim\u003c/b\u003e - The Elder Scrolls V: \u003cb\u003eSkyrim\u003c/b\u003e'") {
+    		assert(testFeeds.entries.head.title === "The Elder Scrolls Official Site | \u003cb\u003eSkyrim\u003c/b\u003e - The Elder Scrolls V: \u003cb\u003eSkyrim\u003c/b\u003e")
+    	}
     }
   }
 }
